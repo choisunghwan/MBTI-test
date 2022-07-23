b2051f99890c269dfa7ed9545774eb50c9823259
ENFJ <33335762+choisunghwan@users.noreply.github.com>
2022-07-23 22:20:16 +0900

    netlify로 배포, 카카오 공유 구현

--- js/start.js
+++ js/start.js
@@ -1,48 +1,31 @@
 const main = document.querySelector("#main"); //index.html에 아이디 main을 정의?
 const qna = document.querySelector("#qna"); // index.html에 아이디 qna 
-const endPoint = 12; //12문제
 const result = document.querySelector("#result");
-const select = [];
 
+const endPoint = 12; //12문제
+const select = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function calResult(){
-    var pintArray = [
-        {name:'mouse', value:0, key:0},
-        {name:'cow', value:0, key:1},
-        {name:'tiger', value:0, key:2},
-        {name:'rabbit', value:0, key:3},
-        {name:'dragon', value:0, key:4},
-        {name:'snake', value:0, key:5},
-        {name:'horse', value:0, key:6},
-        {name:'sheep', value:0, key:7},
-        {name:'monkey', value:0, key:8},
-        {name:'chick', value:0, key:9},
-        {name:'dog', value:0, key:10},
-        {name:'pig', value:0, key:11},
-    ]
-
-    for(let i =0;i<endPoint; i++){
-        var target = qnaList[i].a[select[i]];
-        for(let j =0; j<target.type.length;j++){
-            for(let k=0;k<pointArray.length;k++){
-                if (target.type[j] === pointArray[k].name){
-                    pointArray[k].value +=1;
-                }
-            }
-        }
-    }
-    var resultArray = pointArray.sort(function(a,b){
-        if(a.value > b.value){
-            return -1;
-        }
-        if(a.value<b.value){
-            return 1;
-        }
-        return 0;
-    })
-    let resultword = resultArray[0].key;
-    return resultword;
-}
+    console.log(select);
+    var result = select.indexOf(Math.max(...select));
+    return result;
+  }
+  function setResult(){
+    let point = calResult();
+    const resultName = document.querySelector('.resultname');
+    resultName.innerHTML = infoList[point].name;
+  
+    var resultImg = document.createElement('img');
+    const imgDiv = document.querySelector('#resultImg');
+    var imgURL = 'img/image-' + point + '.png';
+    resultImg.src = imgURL;
+    resultImg.alt = point;
+    resultImg.classList.add('img-fluid');
+    imgDiv.appendChild(resultImg);
+  
+    const resultDesc = document.querySelector('.resultDesc');
+    resultDesc.innerHTML = infoList[point].desc;
+  }
 
 function goResult(){
     qna.style.WebkitAnimation = "fadeOut 1s"; //webkit 을 붙이면 크롬에서 작동하게
@@ -55,10 +38,8 @@ function goResult(){
             qna.style.display = "none"; // main 에 해당하는 부분을 화면에 안나타나도록
             result.style.display = "block"; // qna 에 해당하는 부분을 화면에 나타나도록
         },450)}) //시간 450ms
-        
-        calResult();
 
-    
+        setResult();
 }
 
 function addAnswer(answerText,qIdx, idx){
@@ -73,6 +54,7 @@ function addAnswer(answerText,qIdx, idx){
 
     a.appendChild(answer); //answer라는 버튼이 a에 소속될수있도록 만들어줌
     answer.innerHTML = answerText;
+    
     answer.addEventListener("click", function(){
        var children = document.querySelectorAll('.answerList');
        for(let i=0; i<children.length; i++){
@@ -81,7 +63,10 @@ function addAnswer(answerText,qIdx, idx){
         children[i].style.animation = 'fadeOut 0.5s';
        }
        setTimeout(() => {
-        select[qIdx] = idx;
+        var target = qnaList[qIdx].a[idx].type;
+        for(let i = 0; i < target.length; i++){
+        select[target[i]] += 1;
+      }
         for(let i = 0; i < children.length; i++){
           children[i].style.display = 'none';
         }
